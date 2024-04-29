d1bb6e3ed9c5a28048386c7d4ebc88831707a7f6
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2024-04-29 16:57:45 +0200

    Lesson 20

---
+++ productsservice/src/main/java/com/appsdeveloperblog/estore/productsservice/rest/ProductsController.java
@@ -0,0 +1,10 @@
+package com.appsdeveloperblog.estore.productsservice.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/products")
+@RestController	
+public class ProductsController {
+
+}
