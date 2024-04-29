2ef9a9b3191e06440cbec23ba68af2d9ffeb0f6c
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2024-04-29 17:01:04 +0200

    Lesson 21

--- productsservice/src/main/java/com/appsdeveloperblog/estore/productsservice/rest/ProductsController.java
+++ productsservice/src/main/java/com/appsdeveloperblog/estore/productsservice/rest/ProductsController.java
@@ -1,10 +1,34 @@
 package com.appsdeveloperblog.estore.productsservice.rest;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/products")
 @RestController	
 public class ProductsController {
+
+	@PostMapping
+	public String createProduct() {
+		return "HTTP POST Handled";
+	}
+
+	@GetMapping
+	public String getProduct() {
+		return "HTTP GET Handled";
+	}
+
+	@PutMapping
+	public String updateProduct() {
+		return "HTTP PUT Handled";
+	}
+
+	@DeleteMapping
+	public String deleteProduct() {
+		return "HTTP DELETE Handled";
+	}
 
 }
