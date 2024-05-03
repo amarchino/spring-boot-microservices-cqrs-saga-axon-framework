8b9cd915843f4816309f1c3e1e8ad4a47fe8631f
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2024-05-03 13:49:48 +0200

    Lesson 65

--- productsservice/src/main/java/com/appsdeveloperblog/estore/productsservice/core/data/ProductsRepository.java
+++ productsservice/src/main/java/com/appsdeveloperblog/estore/productsservice/core/data/ProductsRepository.java
@@ -2,7 +2,7 @@ package com.appsdeveloperblog.estore.productsservice.core.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<ProductEntity, String> {
+public interface ProductsRepository extends JpaRepository<ProductEntity, String> {
 
 	ProductEntity findByProductId(String productId);
 	ProductEntity findByProductIdOrTitle(String productId, String title);
--- productsservice/src/main/java/com/appsdeveloperblog/estore/productsservice/query/ProductEventsHandler.java
+++ productsservice/src/main/java/com/appsdeveloperblog/estore/productsservice/query/ProductEventsHandler.java
@@ -1,15 +1,25 @@
 package com.appsdeveloperblog.estore.productsservice.query;
 
 import org.axonframework.eventhandling.EventHandler;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
+import com.appsdeveloperblog.estore.productsservice.core.data.ProductEntity;
+import com.appsdeveloperblog.estore.productsservice.core.data.ProductsRepository;
 import com.appsdeveloperblog.estore.productsservice.core.events.ProductCreatedEvent;
 
+import lombok.RequiredArgsConstructor;
+
 @Component
+@RequiredArgsConstructor
 public class ProductEventsHandler {
+
+	private ProductsRepository productsRepository;
 
 	@EventHandler
 	public void on(ProductCreatedEvent productCreatedEvent) {
-		
+		ProductEntity productEntity = new ProductEntity();
+		BeanUtils.copyProperties(productCreatedEvent, productEntity);
+		productsRepository.save(productEntity);
 	}
 }
