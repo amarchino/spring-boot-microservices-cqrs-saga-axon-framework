5219296a2193a357ae60156c538326b6635a3d0b
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2024-05-03 09:00:19 +0200

    Lesson 52

--- productsservice/src/main/java/com/appsdeveloperblog/estore/productsservice/command/ProductAggregate.java
+++ productsservice/src/main/java/com/appsdeveloperblog/estore/productsservice/command/ProductAggregate.java
@@ -1,5 +1,7 @@
 package com.appsdeveloperblog.estore.productsservice.command;
 
+import java.math.BigDecimal;
+
 import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.spring.stereotype.Aggregate;
 
@@ -12,5 +14,11 @@ public class ProductAggregate {
 	@CommandHandler
 	public ProductAggregate(CreateProductCommand createProductCommand) {
 		// Validate CreateProductCommand
+		if(createProductCommand.getPrice().compareTo(BigDecimal.ZERO) <= 0) {
+			throw new IllegalArgumentException("Price cannot be less than or equal to zero");
+		}
+		if(createProductCommand.getTitle() == null || createProductCommand.getTitle().isBlank()) {
+			throw new IllegalArgumentException("Title cannot be empty");
+		}
 	}
 }
