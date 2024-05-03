ae498d04f042667e4e415243c52fb1c7607e873b
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2024-05-03 13:55:21 +0200

    Lesson 66

--- productsservice/src/main/java/com/appsdeveloperblog/estore/productsservice/query/ProductEventsHandler.java
+++ productsservice/src/main/java/com/appsdeveloperblog/estore/productsservice/query/ProductEventsHandler.java
@@ -14,7 +14,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ProductEventsHandler {
 
-	private ProductsRepository productsRepository;
+	private final ProductsRepository productsRepository;
 
 	@EventHandler
 	public void on(ProductCreatedEvent productCreatedEvent) {
