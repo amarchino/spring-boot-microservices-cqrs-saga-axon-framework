72a61e57aaecae0e42cfac9444bc05653fe54ec4
amarchino <marchino.alessandro@gmail.com>
2024-05-04 14:46:45 +0200

    Lesson 71

--- productsservice/src/main/java/com/appsdeveloperblog/estore/productsservice/query/rest/ProductsQueryController.java
+++ productsservice/src/main/java/com/appsdeveloperblog/estore/productsservice/query/rest/ProductsQueryController.java
@@ -1,5 +1,8 @@
 package com.appsdeveloperblog.estore.productsservice.query.rest;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/products")
 public class ProductsQueryController {
 
+	@GetMapping
+	public List<ProductRestModel> getProducts() {
+		return List.of();
+	}
 }
