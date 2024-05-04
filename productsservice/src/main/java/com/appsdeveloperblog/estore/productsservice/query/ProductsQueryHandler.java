1b0c9278edb5cca45dae160539f6bc1df71d7f97
amarchino <marchino.alessandro@gmail.com>
2024-05-04 15:17:02 +0200

    Lesson 74

--- productsservice/src/main/java/com/appsdeveloperblog/estore/productsservice/query/ProductsQueryHandler.java
+++ productsservice/src/main/java/com/appsdeveloperblog/estore/productsservice/query/ProductsQueryHandler.java
@@ -1,8 +1,13 @@
 package com.appsdeveloperblog.estore.productsservice.query;
 
+import java.util.List;
+
+import org.axonframework.queryhandling.QueryHandler;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
 import com.appsdeveloperblog.estore.productsservice.core.data.ProductsRepository;
+import com.appsdeveloperblog.estore.productsservice.query.rest.ProductRestModel;
 
 import lombok.RequiredArgsConstructor;
 
@@ -11,4 +16,15 @@ import lombok.RequiredArgsConstructor;
 public class ProductsQueryHandler {
 	private final ProductsRepository productsRepository;
 	
+	@QueryHandler
+	public List<ProductRestModel> findProducts(FindProductsQuery query) {
+		return productsRepository.findAll()
+			.stream()
+			.map(pe -> {
+				ProductRestModel prm = new ProductRestModel();
+				BeanUtils.copyProperties(pe, prm);
+				return prm;
+			})
+			.toList();
+	}
 }
