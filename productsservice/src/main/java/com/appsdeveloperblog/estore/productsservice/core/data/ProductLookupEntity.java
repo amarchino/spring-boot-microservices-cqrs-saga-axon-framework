c634515aa295b5d32ad28c95ae86de41da8067d7
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2024-05-06 14:38:51 +0200

    Lesson 88

---
+++ productsservice/src/main/java/com/appsdeveloperblog/estore/productsservice/core/data/ProductLookupEntity.java
@@ -0,0 +1,21 @@
+package com.appsdeveloperblog.estore.productsservice.core.data;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="product_lookup")
+public class ProductLookupEntity implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private String productId;
+	@Column(unique = true)
+	private String title;
+}
