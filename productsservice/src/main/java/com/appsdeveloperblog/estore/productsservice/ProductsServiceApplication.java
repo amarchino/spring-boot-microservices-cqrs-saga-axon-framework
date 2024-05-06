26611daca96fd13b35abb1137316d6721d8b2f0e
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2024-05-06 14:16:07 +0200

    Lesson 85

--- productsservice/src/main/java/com/appsdeveloperblog/estore/productsservice/ProductsServiceApplication.java
+++ productsservice/src/main/java/com/appsdeveloperblog/estore/productsservice/ProductsServiceApplication.java
@@ -1,8 +1,13 @@
 package com.appsdeveloperblog.estore.productsservice;
 
+import org.axonframework.commandhandling.CommandBus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ApplicationContext;
+
+import com.appsdeveloperblog.estore.productsservice.command.interceptors.CreateProductCommandInterceptor;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +15,12 @@ public class ProductsServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductsServiceApplication.class, args);
+	}
+
+	@Autowired
+	void registerProductCommandInterceptor(ApplicationContext context, CommandBus commandBus) {
+		CreateProductCommandInterceptor interceptor = context.getBean(CreateProductCommandInterceptor.class);
+		commandBus.registerDispatchInterceptor(interceptor);
 	}
 
 }
