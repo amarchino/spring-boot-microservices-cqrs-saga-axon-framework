1f51808aaaa649f5e54c529963d0bb634f234a97
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2024-05-07 16:48:06 +0200

    Lesson 99

--- productsservice/src/main/java/com/appsdeveloperblog/estore/productsservice/command/ProductAggregate.java
+++ productsservice/src/main/java/com/appsdeveloperblog/estore/productsservice/command/ProductAggregate.java
@@ -35,7 +35,7 @@ public class ProductAggregate {
 		BeanUtils.copyProperties(createProductCommand, productCreatedEvent);
 		AggregateLifecycle.apply(productCreatedEvent);
 		
-		throw new Exception("An error took place in the CreateProductCommand @CommandHandler");
+		throw new Exception("An error took place in the CreateProductCommand @CommandHandler method");
 	}
 
 	@EventSourcingHandler
