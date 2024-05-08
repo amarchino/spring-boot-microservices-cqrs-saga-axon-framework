da93e4233122f1f975bc3a9a6d95d8a07fdf9244
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2024-05-08 12:30:04 +0200

    Lesson 109

--- ordersservice/src/main/java/com/appsdeveloperblog/estore/ordersservice/saga/OrderSaga.java
+++ ordersservice/src/main/java/com/appsdeveloperblog/estore/ordersservice/saga/OrderSaga.java
@@ -1,7 +1,11 @@
 package com.appsdeveloperblog.estore.ordersservice.saga;
 
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.modelling.saga.SagaEventHandler;
+import org.axonframework.modelling.saga.StartSaga;
 import org.axonframework.spring.stereotype.Saga;
+
+import com.appsdeveloperblog.estore.ordersservice.core.events.OrderCreatedEvent;
 
 import lombok.RequiredArgsConstructor;
 
@@ -9,4 +13,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class OrderSaga {
 	private final transient CommandGateway commandGateway;
+
+	@StartSaga
+	@SagaEventHandler(associationProperty = "orderId")
+	public void handle(OrderCreatedEvent orderCreatedEvent) {
+		
+	}
 }
