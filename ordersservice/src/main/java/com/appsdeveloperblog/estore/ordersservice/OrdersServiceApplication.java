463e1695f6bbd6117ad9d3d704aeeec0a6410ca6
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2024-05-13 09:41:37 +0200

    Lesson 146

--- ordersservice/src/main/java/com/appsdeveloperblog/estore/ordersservice/OrdersServiceApplication.java
+++ ordersservice/src/main/java/com/appsdeveloperblog/estore/ordersservice/OrdersServiceApplication.java
@@ -1,11 +1,17 @@
 package com.appsdeveloperblog.estore.ordersservice;
 
+import org.axonframework.common.transaction.TransactionManager;
+import org.axonframework.config.Configuration;
+import org.axonframework.config.ConfigurationScopeAwareProvider;
 import org.axonframework.config.EventProcessingConfigurer;
+import org.axonframework.deadline.DeadlineManager;
+import org.axonframework.deadline.SimpleDeadlineManager;
 import org.axonframework.eventhandling.PropagatingErrorHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 import com.appsdeveloperblog.estore.core.config.AxonConfig;
@@ -22,5 +28,13 @@ public class OrdersServiceApplication {
 	@Autowired
 	void configure(EventProcessingConfigurer configurer) {
 		configurer.registerListenerInvocationErrorHandler("order-group", conf -> PropagatingErrorHandler.INSTANCE);
+	}
+
+	@Bean
+	DeadlineManager deadlineManager(Configuration configuration, TransactionManager transactionManager) {
+		return SimpleDeadlineManager.builder()
+				.scopeAwareProvider(new ConfigurationScopeAwareProvider(configuration))
+				.transactionManager(transactionManager)
+				.build();
 	}
 }
