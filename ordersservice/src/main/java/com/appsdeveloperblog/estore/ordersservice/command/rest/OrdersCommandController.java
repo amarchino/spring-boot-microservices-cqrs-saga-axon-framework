97890dc20587f527c2ca4ba7c6394d54f71cbf31
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2024-05-13 10:32:22 +0200

    Lesson 154

--- ordersservice/src/main/java/com/appsdeveloperblog/estore/ordersservice/command/rest/OrdersCommandController.java
+++ ordersservice/src/main/java/com/appsdeveloperblog/estore/ordersservice/command/rest/OrdersCommandController.java
@@ -3,6 +3,9 @@ package com.appsdeveloperblog.estore.ordersservice.command.rest;
 import java.util.UUID;
 
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.messaging.responsetypes.ResponseTypes;
+import org.axonframework.queryhandling.QueryGateway;
+import org.axonframework.queryhandling.SubscriptionQueryResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.appsdeveloperblog.estore.ordersservice.command.commands.CreateOrderCommand;
 import com.appsdeveloperblog.estore.ordersservice.model.OrderStatus;
+import com.appsdeveloperblog.estore.ordersservice.model.OrderSummary;
+import com.appsdeveloperblog.estore.ordersservice.query.FindOrderQuery;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -19,19 +24,26 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class OrdersCommandController {
 	private final CommandGateway commandGateway;
+	private final QueryGateway queryGateway;
 
 	@PostMapping
-	public String createOrder(@RequestBody @Valid CreateOrderRestModel createOrderRestModel) {
+	public OrderSummary createOrder(@RequestBody @Valid CreateOrderRestModel createOrderRestModel) {
+		String orderId = UUID.randomUUID().toString();
 		CreateOrderCommand createOrderCommand = CreateOrderCommand.builder()
-			.orderId(UUID.randomUUID().toString())
+			.orderId(orderId)
 			.userId("27b95829-4f3f-4ddf-8983-151ba010e35b")
 			.productId(createOrderRestModel.getProductId())
 			.quantity(createOrderRestModel.getQuantity())
 			.addressId(createOrderRestModel.getAddressId())
 			.orderStatus(OrderStatus.CREATED)
 			.build();
-			
-		String returnValue = commandGateway.sendAndWait(createOrderCommand);
-		return returnValue;
+		
+		try(SubscriptionQueryResult<OrderSummary, OrderSummary> queryResult = queryGateway.subscriptionQuery(
+				new FindOrderQuery(orderId),
+				ResponseTypes.instanceOf(OrderSummary.class),
+				ResponseTypes.instanceOf(OrderSummary.class))) {
+			commandGateway.sendAndWait(createOrderCommand);
+			return queryResult.updates().blockFirst();
+		}
 	}
 }
